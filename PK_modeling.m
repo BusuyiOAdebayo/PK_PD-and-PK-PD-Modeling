a9bbaa78a5d79c3da6de08eae20b2f8d356252e3
Busuyi O. Adebayo <90490605+BusuyiOAdebayo@users.noreply.github.com>
2023-06-13 19:01:34 -0500

    Update PK_modeling.m

--- PK_modeling.m
+++ PK_modeling.m
@@ -1,14 +1,12 @@
 % parameter values
-
-% Define you adsorption function:
-function dXindt = myAds(t,Xin,Xininit,Kads)
-  %dXindt = zeros(1,1);
-  dXindt = -Kads*Xin
-end
+function X = simX(Xin_init,Xinit,Kads,Keli,tspan,optionsSim)
+myPKSingleCompt_h = @(t,X) myPKSingleCompt(t,X,Keli,Xin_init,Kads);
+[t,X] = ode45(myPKSingleCompt_h,tspan,Xinit,optionsSim);
 
 % Define compartment function:
-function dXdt = myComp1(t,X,Xinit,Keli)
+function dXdt = myPKSingleCompt(t,X,Keli,Xin_init,Kads)
   %dXdt = zeros(1,1)
-  Xin_h = @(t,Xin) myAds(t,Xin,Xininit,Kads);
+  Xin_h = @(t,Xin) myPKAds(t,Xin,Xininit,Kads);
   dXdt = Kads*Xin_h-Keli*X;
+end
 end
